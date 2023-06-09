cb4a5a8816332994d2e95db1ed3cab3f52ac159f
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-06-09 04:28:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,77 +2,52 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
+
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("button clicked");
-  
-    formValidation();
-  });
-  
-  let formValidation = () => {};
-  let formValidation = () => {
-    if (input.value === "") {
-      msg.innerHTML = "Post cannot be blank";
-      console.log("failure");
-    } else {
-      console.log("successs");
-      msg.innerHTML = "";
-    }
-  };
+  e.preventDefault();
+  console.log("button clicked");
+  formValidation();
+});
 
-  let data = {};
-let acceptData = () => {};
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be blank";
+    console.log("failure");
+  } else {
+    console.log("successs");
+    msg.innerHTML = "";
+    acceptData();
+  }
+};
+
+let data = {};
+
 let acceptData = () => {
-    data["text"] = input.value;
-    console.log(data);
-  };
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
+};
 
-  let formValidation = () => {
-    if (input.value === "") {
-      // Other codes are here
-    } else {
-      // Other codes are here
-      acceptData();
-    }
-  };
+let createPost = () => {
+  posts.innerHTML += `
+  <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+  input.value = "";
+};
 
-  let createPost = () => {
-    posts.innerHTML += '';
-  };
+let deletePost = (e) => {
+  e.parentElement.parentElement.remove();
+};
 
-  let createPost = () => {
-    posts.innerHTML += `<div>
-      <p>${data.text}</p>
-      <span class="options">
-        <i onClick="editPost(this)" class="fas fa-edit"></i>
-        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-      </span>
-    </div>`;
-    input.value = "";
-  };
-
-  let acceptData = () => {
-    // Other codes are here
-  
-    createPost();
-  };
-
-  // deleting posts
-  let deletePost = (e) => {};
-  <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-  let deletePost = (e) => {
-    e.parentElement.parentElement.remove();
-  };
-
-// Editing posts
-  let editPost = (e) => {};
-  <i onClick="editPost(this)" class="fas fa-edit"></i>
-  let editPost = (e) => {
-    input.value = e.parentElement.previousElementSibling.innerHTML;
-    e.parentElement.parentElement.remove();
-  };
+let editPost = (e) => {
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
 
 
-
-
-  
